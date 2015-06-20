fb140080564c57a112a7da6ea69a5679579efeef
Benjamin Patch <ben@cascadingmedia.com>
2015-06-20 16:49:01 -0500

    Update configuration for production.

--- config.rb
+++ config.rb
@@ -1,4 +1,9 @@
+set :url_root, 'https://southwestrecording.com'
+
 activate :directory_indexes
+
+activate :search_engine_sitemap
+
 set :partials_dir, 'partials'
 
 ###
@@ -94,18 +99,40 @@ set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
+  # Enable gzip
+  activate :gzip
+
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
-  # activate :asset_hash
+  activate :asset_hash
 
   # Use relative URLs
   activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
+
+  activate :minify_html do |html|
+    html.remove_multi_spaces        = true   # Remove multiple spaces
+    html.remove_comments            = true   # Remove comments
+    html.remove_intertag_spaces     = false  # Remove inter-tag spaces
+    html.remove_quotes              = true   # Remove quotes
+    html.simple_doctype             = false  # Use simple doctype
+    html.remove_script_attributes   = true   # Remove script attributes
+    html.remove_style_attributes    = true   # Remove style attributes
+    html.remove_link_attributes     = true   # Remove link attributes
+    html.remove_form_attributes     = false  # Remove form attributes
+    html.remove_input_attributes    = true   # Remove input attributes
+    html.remove_javascript_protocol = true   # Remove JS protocol
+    html.remove_http_protocol       = false  # Remove HTTP protocol
+    html.remove_https_protocol      = false  # Remove HTTPS protocol
+    html.preserve_line_breaks       = false  # Preserve line breaks
+    html.simple_boolean_attributes  = true   # Use simple boolean attributes
+    # html.preserve_patterns          = nil    # Patterns to preserve
+  end
 end
